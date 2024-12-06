49dc8d4d4dc51dc588377d91922b7bf7cf886bb6
highself <89912995+xvsvg@users.noreply.github.com>
2024-12-06 19:01:18 +0300

    Update TaskStatusProjection.kt

--- src/main/kotlin/ru/quipy/projections/TaskStatusProjection.kt
+++ src/main/kotlin/ru/quipy/projections/TaskStatusProjection.kt
@@ -12,7 +12,8 @@ data class TaskStatusProjection(
     val projectId: UUID = UUID.randomUUID(),
     val rColor: Int = 0,
     val gColor: Int = 0,
-    val bColor: Int = 0
+    val bColor: Int = 0,
+    val priority: Int = 0
 ) {
-    constructor() : this(UUID.randomUUID(), "", UUID.randomUUID(), 0, 0, 0)
+    constructor() : this(UUID.randomUUID(), "", UUID.randomUUID(), 0, 0, 0, 0)
 }
